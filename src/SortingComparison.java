0f3b5a01b92f1f63e26e5eff3d5c4586afc3bfa8
Jake Chiang <jakewchiang@gmail.com>
2018-01-23 22:48:02 -0800

    Added reverse order shuffling.

--- src/SortingComparison.java
+++ src/SortingComparison.java
@@ -4,42 +4,33 @@ import java.util.Collections;
 import java.util.List;
 
 public class SortingComparison {
-    public enum ColorScheme {
-        RAINBOW
-    }
-
-    private enum ShuffleType {
-        RANDOM, REVERSE_SORTED
-    }
-
     private static final int GRID_WIDTH = 60;
     private static final int GRID_HEIGHT = 60;
     private static final int CELL_SIZE = 10;
     private SimpleGrid grid;
     private ColorScheme colorScheme;
-
     public SortingComparison(ColorScheme colorScheme) {
         this.colorScheme = colorScheme;
         grid = new SimpleGrid(GRID_WIDTH, GRID_HEIGHT, CELL_SIZE, 0, "Sorting Algorithm Comparison");
 
-        for(int i = 0; i < GRID_HEIGHT; i++) {
+        for (int i = 0; i < GRID_HEIGHT; i++) {
             grid.setColor(i, valueToColor(i, 0, GRID_HEIGHT));
             grid.fillRow(i, i);
         }
     }
 
     private Color valueToColor(int value, int minValue, int maxValue) {
-        if(minValue >= maxValue) {
+        if (minValue >= maxValue) {
             throw new IllegalArgumentException("Minimum value must be less than maximum value.");
         }
-        if(value < minValue || value > maxValue) {
+        if (value < minValue || value > maxValue) {
             throw new IllegalArgumentException("Value must be within min and max range.");
         }
 
         int range = maxValue - minValue;
-        float trueValue = ((float)(value - minValue)) / range;
+        float trueValue = ((float) (value - minValue)) / range;
 
-        switch(colorScheme) {
+        switch (colorScheme) {
             case RAINBOW:
                 return new Color(Color.HSBtoRGB(trueValue, 1.f, 1.f));
         }
@@ -51,18 +42,22 @@ public class SortingComparison {
         List<Integer> columnValues = new ArrayList<>();
         switch (shuffleType) {
             case RANDOM:
-                for(int i = 0; i < GRID_HEIGHT; i++) {
+                for (int i = 0; i < GRID_HEIGHT; i++) {
                     columnValues.add(i);
                 }
-                for(int x = 0; x < GRID_WIDTH; x++) {
-                    Collections.shuffle(columnValues);
-                    for(int y = 0; y < GRID_HEIGHT; y++) {
-                        grid.set(x, y, columnValues.get(y));
-                    }
-                }
+                Collections.shuffle(columnValues);
                 break;
             case REVERSE_SORTED:
+                for (int i = 0; i < GRID_HEIGHT; i++) {
+                    columnValues.add(GRID_HEIGHT - 1 - i);
+                }
                 break;
+        }
+
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            for (int y = 0; y < GRID_HEIGHT; y++) {
+                grid.set(x, y, columnValues.get(y));
+            }
         }
     }
 
@@ -71,5 +66,13 @@ public class SortingComparison {
         while (true) {
 
         }
+    }
+
+    public enum ColorScheme {
+        RAINBOW
+    }
+
+    private enum ShuffleType {
+        RANDOM, REVERSE_SORTED
     }
 }
