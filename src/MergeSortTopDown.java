bdb63ce9618e05ac4fc93e5683056412eb6e2c70
Jake Chiang <jakewchiang@gmail.com>
2018-01-24 21:22:21 -0800

    Added documentation for top down merge sort.

--- src/MergeSortTopDown.java
+++ src/MergeSortTopDown.java
@@ -11,66 +11,90 @@ public class MergeSortTopDown extends Sort {
      */
     @Override
     public void run() {
+        // The array to sort
         int[] source = new int[length()];
+        // The work array
         int[] work = new int[length()];
         for (int i = 0; i < work.length; i++) {
             source[i] = get(i);
             work[i] = get(i);
         }
-        split(work, 0, length(), source);
+        // Sort data from work to source
+        splitMerge(work, 0, length(), source);
     }
 
     /**
-     * TODO
+     * Sorts a segment of a given data series.
      *
-     * @param source
-     * @param start
-     * @param end
-     * @param work
+     * @param source The array with the data to sort.
+     * @param start  The index of the first element in the segment to sort.
+     * @param end    The index after the last element in the segment to sort.
+     * @param result The array to store the sorting results into. Should contain
+     *               the same data as the source array.
      */
-    private void split(int[] source, int start, int end, int[] work) {
+    private void splitMerge(int[] source, int start, int end, int[] result) {
+        // Stop when only 1 element is left
         if (end - start < 2) {
             return;
         }
+        // Sort the left and right segments
         int middle = (end + start) / 2;
-        split(work, start, middle, source);
-        split(work, middle, end, source);
+        splitMerge(result, start, middle, source);
+        splitMerge(result, middle, end, source);
 
-        merge(source, start, middle, end, work);
+        // Merge the left and right segments now that they're sorted
+        merge(source, start, middle, end, result);
     }
 
     /**
-     * TODO
+     * Merges two contiguous sorted segments into a single sorted segment.
+     * <p>
+     * [start, middle) is the first segment.
+     * [middle, end) is the second segment.
      *
-     * @param source
-     * @param start
-     * @param middle
-     * @param end
-     * @param work
+     * @param source The array with the data segments to merge. Each segment
+     *               must be sorted.
+     * @param start  Index of the first element in the first segment.
+     * @param middle Index after the last element in the first segment and the
+     *               first element in the second segment.
+     * @param end    Index after the last element in the second segment.
+     * @param result The array to store the results into. The sorted segments
+     *               will be merged into a single segment [start, end) in this
+     *               array.
      */
-    private void merge(int[] source, int start, int middle, int end, int[] work) {
+    private void merge(int[] source, int start, int middle, int end, int[] result) {
+        // Keep track of the next element to process in each segment
         int i = start;
         int j = middle;
 
+        // Merge the segments and store into result
         for (int k = start; k < end; k++) {
+            // Sort elements from the segments into the result. If one segment
+            // runs out of elements, add the rest of the elements of the other.
             if (i < middle && (j >= end || source[i] <= source[j])) {
-                work[k] = source[i];
+                // If the next element in the first segment is less than the
+                // next element in the second segment, add it to the result.
+                result[k] = source[i];
                 i++;
             } else {
-                work[k] = source[j];
+                // If instead the next element in the second segment is less,
+                // add it to the result
+                result[k] = source[j];
                 j++;
             }
-            updateGrid(work, start, end);
+            // Update visualization
+            updateGrid(result, start, end);
             delay();
         }
     }
 
     /**
-     * TODO
+     * Updates a segment of the grid based on the corresponding values in a
+     * given array.
      *
-     * @param values
-     * @param start
-     * @param end
+     * @param values The array with the values to update the grid with.
+     * @param start  Index of the first element in the segment.
+     * @param end    Index after the last element in the segment.
      */
     private void updateGrid(int[] values, int start, int end) {
         for (int i = start; i < end; i++) {
