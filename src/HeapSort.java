d34b90748026d9c48677e30bc7c79889d89b27d1
Jake Chiang <jakewchiang@gmail.com>
2018-01-26 22:22:08 -0800

    Added documentation for heap sort.

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -13,12 +13,16 @@ public class HeapSort extends Sort {
      */
     @Override
     public void run() {
+        // Make a max heap
         heapify();
 
         int end = length() - 1;
         while (end > 0) {
+            // Move the largest item to the end
             swap(end, 0);
+            // "Shorten" the heap so already sorted elements stay that way
             end--;
+            // Repair the heap, since a value was swapped to the top
             siftDown(0, end);
 
             delay();
@@ -35,32 +39,56 @@ public class HeapSort extends Sort {
         return (childIndex - 1) / DEGREE;
     }
 
+    /**
+     * Turns the data into a max heap using Floyd's heap construction method.
+     */
     private void heapify() {
+        // Begin with start as the last parent node, since it would be
+        // unnecessary to sift leaf nodes on their own.
         int start = parentIndex(length() - 1);
 
+        // Repair all sub-heaps
         while (start >= 0) {
             siftDown(start, length() - 1);
             start--;
         }
     }
 
+    /**
+     * Repair the heap from a given start index to a given end index, swapping
+     * out of place elements down until they're in a proper place.
+     *
+     * @param start The index of the first element to start repairing the heap
+     *              from.
+     * @param end   The index of the last element in end repairing the heap at.
+     */
     private void siftDown(int start, int end) {
+        // Begin at the top of this heap
         int root = start;
 
+        // Repeat as long as at least one child still exists
         while (root * DEGREE + 1 <= end) {
-            int child = root * DEGREE + 1;
+            int leftChild = root * DEGREE + 1;
+            // Holds the index of the node to be swapped with root
             int swap = root;
 
-            if (get(swap) < get(child)) {
-                swap = child;
+            // If the left or right child is greater than root, set swap to
+            // whichever is the greater.
+            // Compare leftChild to swap
+            if (get(swap) < get(leftChild)) {
+                swap = leftChild;
+            }
+            // Compare rightChild (leftChild + 1) to swap, if it exists
+            if (leftChild + 1 <= end && get(swap) < get(leftChild + 1)) {
+                swap = leftChild + 1;
             }
 
-            if (child + 1 <= end && get(swap) < get(child + 1)) {
-                swap = child + 1;
-            }
             if (swap == root) {
+                // If nothing needs to be swapped, then the heap is repaired
                 return;
             } else {
+                // Otherwise, swap the root down and repeat the process for the
+                // next heap
                 swap(root, swap);
                 root = swap;
             }
